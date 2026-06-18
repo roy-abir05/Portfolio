f1f90d44e06bb272682a6cc517b0fb8456972951
roy-abir05 <royabirdhn@gmail.com>
2026-06-18 17:45:21 +0530

    chore: add more engineering quotes

--- lib/engineering-quotes.ts
+++ lib/engineering-quotes.ts
@@ -1,4 +1,5 @@
 export const quotes = [
+  // --- OGs ---
   { text: "Talk is cheap. Show me the code.", author: "Linus Torvalds" },
   {
     text: "Premature optimization is the root of all evil.",
@@ -19,5 +20,35 @@ export const quotes = [
   {
     text: "The most effective debugging tool is still careful thought, coupled with judiciously placed print statements.",
     author: "Brian Kernighan",
+  },
+
+  // --- The Systems & C++ Expansion ---
+  {
+    text: "Bad programmers worry about the code. Good programmers worry about data structures and their relationships.",
+    author: "Linus Torvalds",
+  },
+  {
+    text: "C makes it easy to shoot yourself in the foot; C++ makes it harder, but when you do it blows your whole leg off.",
+    author: "Bjarne Stroustrup",
+  },
+  {
+    text: "There are only two hard things in Computer Science: cache invalidation and naming things.",
+    author: "Phil Karlton",
+  },
+  {
+    text: "A distributed system is one in which the failure of a computer you didn't even know existed can render your own computer unusable.",
+    author: "Leslie Lamport",
+  },
+  {
+    text: "Sometimes, the elegant implementation is just a function. Not a method. Not a class. Not a framework. Just a function.",
+    author: "John Carmack",
+  },
+  {
+    text: "Data dominates. If you've chosen the right data structures and organized things well, the algorithms will almost always be self-evident.",
+    author: "Rob Pike",
+  },
+  {
+    text: "Adding manpower to a late software project makes it later.",
+    author: "Fred Brooks",
   },
 ];
